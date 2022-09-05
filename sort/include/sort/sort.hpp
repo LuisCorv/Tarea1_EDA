8d060b24469bbdd473515d38b187585061969b07
macaferrari <mdferrari@miuandes.cl>
2022-09-05 15:33:55 -0300

    insetion sort agregado

--- sort/include/sort/sort.hpp
+++ sort/include/sort/sort.hpp
@@ -5,6 +5,7 @@ namespace sort {
 	//♦♦♦♦♦Algoritmos de Ordenacion Genericos♦♦♦♦♦♦
 
 	//Crear Insercion Sort
+	void insertionSort(int arr[], int n);
 	void selectionSort(float* A, int n);
 	int split_qs(float* A, int i, int j);
 	void quickSort(float* A, int i, int j);
--- sort/src/sort.cpp
+++ sort/src/sort.cpp
@@ -4,6 +4,18 @@
 
 namespace sort{
 	//Crear el metodo de Insercion Sort
+	void insertionSort(int arr[], int n) {
+		for (int i = 1; i < n; i++){
+			int k = arr[i];
+			int j = i -1;
+
+			while (k < arr[j] && j >= 0) {
+				arr[j+1] = arr[j];
+				--j;
+			}
+			arr[j + 1] = k;
+		}
+	}
 
 	void selectionSort(float* A, int n){
 		int smallest = 0;
